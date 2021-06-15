73adfa04681bcb0a0fa1b21737c10d572d2c13fc
ShootMe <birreller@gmail.com>
2021-06-15 12:08:23 -0500

    Fix pointers

--- SplitterMemory.cs
+++ SplitterMemory.cs
@@ -2,8 +2,8 @@
 using System.Diagnostics;
 namespace LiveSplit.Tinertia {
 	public partial class SplitterMemory {
-		private static ProgramPointer CgGame = new ProgramPointer(AutoDeref.Single, new ProgramSignature(PointerVersion.V1, "558BEC83EC288B05????????83EC086A0050E8????????83C41085C0741D8B05", 8));
-		private static ProgramPointer Tinertia = new ProgramPointer(AutoDeref.Single, new ProgramSignature(PointerVersion.V1, "558BEC53575683EC5C8B7D088B05????????83EC0C50E8????????83C41085C00F84", 14));
+		private static ProgramPointer CgGame = new ProgramPointer(new FindPointerSignature(PointerVersion.All, AutoDeref.Single, "558BEC83EC288B05????????83EC086A0050E8????????83C41085C0741D8B05", 8));
+		private static ProgramPointer Tinertia = new ProgramPointer(new FindPointerSignature(PointerVersion.All, AutoDeref.Single, "68????????57E8????????83C41085FF0F84????????83EC0C68????????E8????????83C410897810C74014????????C7401C????????C7400C????????83EC0C50E8????????83C41083EC0C6AFF", 1));
 		public Process Program { get; set; }
 		public bool IsHooked { get; set; } = false;
 		private DateTime lastHooked;
@@ -160,120 +160,6 @@ namespace LiveSplit.Tinertia {
 			if (Program != null) {
 				Program.Dispose();
 			}
-		}
-	}
-	public enum PointerVersion {
-		V1
-	}
-	public enum AutoDeref {
-		None,
-		Single,
-		Double
-	}
-	public class ProgramSignature {
-		public PointerVersion Version { get; set; }
-		public string Signature { get; set; }
-		public int Offset { get; set; }
-		public ProgramSignature(PointerVersion version, string signature, int offset) {
-			Version = version;
-			Signature = signature;
-			Offset = offset;
-		}
-		public override string ToString() {
-			return Version.ToString() + " - " + Signature;
-		}
-	}
-	public class ProgramPointer {
-		private int lastID;
-		private DateTime lastTry;
-		private ProgramSignature[] signatures;
-		private int[] offsets;
-		public IntPtr Pointer { get; private set; }
-		public PointerVersion Version { get; private set; }
-		public AutoDeref AutoDeref { get; private set; }
-
-		public ProgramPointer(AutoDeref autoDeref, params ProgramSignature[] signatures) {
-			AutoDeref = autoDeref;
-			this.signatures = signatures;
-			lastID = -1;
-			lastTry = DateTime.MinValue;
-		}
-		public ProgramPointer(AutoDeref autoDeref, params int[] offsets) {
-			AutoDeref = autoDeref;
-			this.offsets = offsets;
-			lastID = -1;
-			lastTry = DateTime.MinValue;
-		}
-
-		public T Read<T>(Process program, params int[] offsets) where T : struct {
-			GetPointer(program);
-			return program.Read<T>(Pointer, offsets);
-		}
-		public string Read(Process program, params int[] offsets) {
-			GetPointer(program);
-			return program.Read((IntPtr)program.Read<uint>(Pointer, offsets));
-		}
-		public byte[] ReadBytes(Process program, int length, params int[] offsets) {
-			GetPointer(program);
-			return program.Read(Pointer, length, offsets);
-		}
-		public void Write<T>(Process program, T value, params int[] offsets) where T : struct {
-			GetPointer(program);
-			program.Write<T>(Pointer, value, offsets);
-		}
-		public void Write(Process program, byte[] value, params int[] offsets) {
-			GetPointer(program);
-			program.Write(Pointer, value, offsets);
-		}
-		public IntPtr GetPointer(Process program) {
-			if (program == null) {
-				Pointer = IntPtr.Zero;
-				lastID = -1;
-				return Pointer;
-			} else if (program.Id != lastID) {
-				Pointer = IntPtr.Zero;
-				lastID = program.Id;
-			}
-
-			if (Pointer == IntPtr.Zero && DateTime.Now > lastTry.AddSeconds(1)) {
-				lastTry = DateTime.Now;
-
-				Pointer = GetVersionedFunctionPointer(program);
-				if (Pointer != IntPtr.Zero) {
-					if (AutoDeref != AutoDeref.None) {
-						Pointer = (IntPtr)program.Read<uint>(Pointer);
-						if (AutoDeref == AutoDeref.Double) {
-							if (MemoryReader.is64Bit) {
-								Pointer = (IntPtr)program.Read<ulong>(Pointer);
-							} else {
-								Pointer = (IntPtr)program.Read<uint>(Pointer);
-							}
-						}
-					}
-				}
-			}
-			return Pointer;
-		}
-		private IntPtr GetVersionedFunctionPointer(Process program) {
-			if (signatures != null) {
-				MemorySearcher searcher = new MemorySearcher();
-				searcher.MemoryFilter = delegate (MemInfo info) {
-					return (info.State & 0x1000) != 0 && (info.Protect & 0x40) != 0 && (info.Protect & 0x100) == 0;
-				};
-				for (int i = 0; i < signatures.Length; i++) {
-					ProgramSignature signature = signatures[i];
-
-					IntPtr ptr = searcher.FindSignature(program, signature.Signature);
-					if (ptr != IntPtr.Zero) {
-						Version = signature.Version;
-						return ptr + signature.Offset;
-					}
-				}
-
-				return IntPtr.Zero;
-			}
-
-			return (IntPtr)program.Read<uint>(program.MainModule.BaseAddress, offsets);
 		}
 	}
 	public enum CgTimerState {
